4977c168f18ad305dd938ec073d806391c399f76
mritd <mritd1234@gmail.com>
2017-01-17 08:11:09 +0800

    删除ohmyzsh

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+tee /usr/local/bin/proxy <<EOF
+#!/bin/bash
+http_proxy=http://192.168.1.120:8123 https_proxy=http://192.168.1.120:8123 \$*
+EOF
+
+chmod +x /usr/local/bin/proxy
+
 yum update -y
 yum install tmux wget lrzsz vim net-tools zsh bind-utils git -y
-git clone https://github.com/mritd/shell_scripts.git /root/shell_scripts
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
