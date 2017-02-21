70e5226b8326a9b05185f5444f04550e81444925
mritd <mritd1234@gmail.com>
2017-02-21 20:43:59 +0800

    增加 docker 代理 和 devicemapper 设置

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -19,8 +19,9 @@ yum install docker-engine tmux wget lrzsz vim net-tools zsh bind-utils git epel-
 
 sudo su root && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo su root && git clone https://github.com/mritd/shell_scripts.git /root/shell_scripts
+sudo su root && bash ~/shell_scripts/docker_devicemapper.sh /dev/sda
+sudo su root && bash ~/shell_scripts/docker_proxy.sh centos
 sudo su root && mkdir ~/.ssh && cat /vagrant/authorized_keys >> ~/.ssh/authorized_keys
 sed -i 's/^#RSAAuthentication.*/RSAAuthentication\ yes/g' /etc/ssh/sshd_config
 sed -i 's/^#PubkeyAuthentication.*/PubkeyAuthentication\ yes/g' /etc/ssh/sshd_config
 sed -i 's/^PasswordAuthentication.*/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
-
