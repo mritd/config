5c9ed7cf433d020ed2ef8033fe642a0d61f734d1
mritd <mritd@mritd.me>
2017-02-23 10:38:41 +0800

    加大内存

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -4,7 +4,7 @@ set -e
 
 tee /usr/local/bin/proxy <<EOF
 #!/bin/bash
-http_proxy=http://192.168.1.120:8123 https_proxy=http://192.168.1.120:8123 \$*
+http_proxy=http://172.16.0.19:8123 https_proxy=http://172.16.0.19:8123 \$*
 EOF
 
 chmod +x /usr/local/bin/proxy
@@ -20,7 +20,12 @@ yum install docker-engine tmux wget lrzsz vim net-tools zsh bind-utils git epel-
 sudo su root && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo su root && git clone https://github.com/mritd/shell_scripts.git /root/shell_scripts
 sudo su root && bash ~/shell_scripts/docker_devicemapper.sh /dev/sda
-sudo su root && bash ~/shell_scripts/docker_proxy.sh centos
+sudo su root && mkdir /etc/systemd/system/docker.service.d || true && \
+tee /etc/systemd/system/docker.service.d/socks5-proxy.conf <<-EOF
+[Service]
+Environment="ALL_PROXY=http://10.10.1.20:8123"
+EOF
+
 sudo su root && mkdir ~/.ssh && cat /vagrant/authorized_keys >> ~/.ssh/authorized_keys
 sed -i 's/^#RSAAuthentication.*/RSAAuthentication\ yes/g' /etc/ssh/sshd_config
 sed -i 's/^#PubkeyAuthentication.*/PubkeyAuthentication\ yes/g' /etc/ssh/sshd_config
