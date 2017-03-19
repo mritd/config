38efe06f543364f43fbf17eb29da3b43d3dfe8a2
mritd <mritd1234@gmail.com>
2017-03-19 06:02:55 -0400

    更换代理地址

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -4,7 +4,7 @@ set -e
 
 tee /usr/local/bin/proxy <<EOF
 #!/bin/bash
-http_proxy=http://192.168.1.120:8123 https_proxy=http://192.168.1.120:8123 \$*
+http_proxy=http://192.168.1.110:8123 https_proxy=http://192.168.1.110:8123 \$*
 EOF
 
 chmod +x /usr/local/bin/proxy
